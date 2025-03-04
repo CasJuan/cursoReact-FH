6b57d76325be844ff2cc57f38798e3afe28d3482
JCastro <castrojuanig25@gmail.com>
2025-03-04 19:55:26 -0300

    [add]Tarea - Componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { HelloWorld } from './HelloWorld';
 import { FirstApp } from './FirstApp';
 
 import './style.css';
+import { CounterApp } from './CounterApp';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorld/>
-        <FirstApp title={"Hola soy Goku"} subTitle={123}/>
+        {/* <HelloWorld/> */}
+        {/* <FirstApp title={"Hola soy Goku"} subTitle={123}/> */}
+        <CounterApp value = {12}/>
     </React.StrictMode>
 )
