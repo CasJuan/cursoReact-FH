06518a743796bfed6a80ba721fa103c3ab3cce17
JCastro <castrojuanig25@gmail.com>
2025-03-09 18:40:56 -0300

    [add] Jest - Expect - toBe

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,6 +1,22 @@
 
-test('esta prueba no debe fallar', () => { 
-    if( 0 === 1 ){
-        throw new Error('esta prueba debe fallar')
-    }
- })
+describe('Pruebas en el archivo demo.test.js', () => {
+
+    test('esta prueba no debe fallar', () => { 
+        
+        // I Inicialización
+    
+        const message1 = 'Hola Mundo';
+    
+        //E Estimulo
+    
+        const message2 = message1.trim();
+    
+        //Observar el comportamiento esperado
+    
+        //expect(message1).toBe(message2);
+        expect(message1).toBe(message2);
+    
+     })
+});
+
+
