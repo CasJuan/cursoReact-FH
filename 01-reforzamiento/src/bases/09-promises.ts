26473a647a5a37527ff5e662a8e825520bd47e2b
JuanCas <jcastro@colegium.com>
2025-11-17 16:29:29 -0300

    promesas

---
+++ 01-reforzamiento/src/bases/09-promises.ts
@@ -0,0 +1,20 @@
+
+const myPromise = new Promise<number>((resolve, reject) => {
+    setTimeout(() => {
+        //! Quiero mi di
+        reject('Mi amigo se perdio')
+    }, 2000)
+})
+
+
+myPromise
+    .then(
+    (myMoney) => {
+        console.log(`Tengo mi dinero ${myMoney}`)
+    })
+    .catch(reason => {
+        console.warn(reason);
+    })
+    .finally( () => {
+        console.log('Pues a seguir con mi vida')
+    })
