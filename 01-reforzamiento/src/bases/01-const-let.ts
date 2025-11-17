e5009861787457a4c02081ec4237fb0a1ab281d6
JuanCas <jcastro@colegium.com>
2025-11-17 11:13:48 -0300

    inicializacion de reforzamiento, variables y constantes

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,17 @@
+
+// variables 
+
+/* 
+    no usar mas var, usar como buena practica const si no vamos a cambiar el valor
+    si sabemos que cambiaria de valor usar let
+*/ 
+
+let firstName = 'Juan';
+const lastName:string = 'Castro';
+
+let diceNumber = 5;
+
+const containsLetterO = lastName.includes('o');
+
+console.log({containsLetterO, diceNumber, firstName, lastName})
+
