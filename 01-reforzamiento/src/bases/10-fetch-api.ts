5ce00fc3c0941b4817a80098e209b8eb4738ea42
JuanCas <jcastro@colegium.com>
2025-11-18 16:23:50 -0300

    interfaces y optimizaciones

--- 01-reforzamiento/src/bases/10-fetch-api.ts
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,3 +1,4 @@
+import type { GiphyRanomResponse } from "./data/giphy.response";
 
 const API_KEY = '0nkT8xdzw3P8Ivy9aS1rHMcrtgUJkJ4M';
 
@@ -6,18 +7,22 @@ const myRequest = fetch(
     `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`
 )
 
+
+const createImageInsideDOM = (url:string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+
+    document.body.append(imgElement);
+}
+
 myRequest
     .then((response) => 
         response.json()
     )
-    .then((data) => {
-
-        const imageUrl = data.data.images.original.url;
-        console.log(imageUrl)
-        const imgElement = document.createElement('img');
-        imgElement.src = imageUrl;
-
-        document.body.append(imgElement);
+    .then(({data}: GiphyRanomResponse) => {
+        const imageUrl = data.images.original.url;
+        /* const imageUrl = data.data.images.original.url; */
+        createImageInsideDOM(imageUrl);
     })
     .catch((err) => {
         console.log(err)
