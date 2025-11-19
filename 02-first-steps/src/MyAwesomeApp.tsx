0f3f4c61f9224f49da828cf01c38c0edc3b0ebc0
JuanCas <jcastro@colegium.com>
2025-11-19 11:14:22 -0300

    impresion de variables

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,9 +1,29 @@
 //TAREA
 export const MyAwesomeApp = () => {
+
+    const firstName = 'Juan';
+    const lastName = 'Castro';
+
+    const favoriteGames = ['Elden Ring', 'Smash', 'Metal gear'];
+    const isActive = false;
+
+    const address = {
+      zipCode: 'ABC-123',
+      country: 'Canada'
+    }
+
   return (
     <>
-        <h1>Fernando</h1>
-        <h3>Herrera</h3>
+        <h1>{firstName}</h1>
+        <h3>{lastName}</h3>
+
+        <p>{favoriteGames.join(', ')}</p>
+
+        <h1>{isActive ? 'Activo' : 'No activo'}</h1>
+
+        <p>
+          {JSON.stringify(address)}
+        </p>
     </>
   )
 }
