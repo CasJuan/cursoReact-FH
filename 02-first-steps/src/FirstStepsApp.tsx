05dbf9c70337b1957755fa5ac842e7fb69caa961
JuanCas <jcastro@colegium.com>
2025-11-19 11:47:11 -0300

    propiedades del componente

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -4,7 +4,9 @@ export function FirstStepsApp () {
     return(
         <>
             <h1>Carrito de compras</h1>
-            <ItemCounter />
+            <ItemCounter name="Nintendo Switch 2" quantity = {2}  />
+            <ItemCounter name="Play 2" quantity = {5}/>
+            <ItemCounter name="XBOX 360" quantity = {3}/>
         </>
     )
-}
+} 
