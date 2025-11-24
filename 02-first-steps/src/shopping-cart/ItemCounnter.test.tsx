b4fb39e2d0cb21688a959d82348b0b9f1748c208
JuanCas <jcastro@colegium.com>
2025-11-24 02:12:30 -0300

    test itemCounter, disparar eventos

---
+++ 02-first-steps/src/shopping-cart/ItemCounnter.test.tsx
@@ -0,0 +1,69 @@
+import { fireEvent, getByText, render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { ItemCounter } from "./ItemCounter";
+
+
+describe('ItemCounter', () => {
+
+    test('should render with default values', () => { 
+
+        const name = 'Contol de nintendo'
+        render(<ItemCounter name={name} />)
+
+        expect(screen.getByText(name)).toBeDefined();
+        expect(screen.getByText(name)).not.toBeNull();
+
+    })
+
+    test('should render with custom quantity', () => { 
+
+        const name = 'Contol de nintendo'
+        const quantiy = 10;
+
+        render(<ItemCounter name={name} quantity={quantiy}/>)
+
+        expect(screen.getByText(quantiy)).toBeDefined();
+     });
+
+
+    test('should increase count when +1 button is pressed', () => { 
+        
+        render(<ItemCounter name={'Test item'} quantity={1} />)
+
+        const [buttonAdd] = screen.getAllByRole('button');
+        
+        fireEvent.click(buttonAdd);
+
+        expect(screen.getByText('2')).toBeDefined();
+
+    })
+
+    test('should increase count when -1 button is pressed', () => { 
+        
+        const quantity = 5;
+        
+        render(<ItemCounter name={'Test item'} quantity={quantity} />)
+
+        const [, buttonRest] = screen.getAllByRole('button');
+        
+        fireEvent.click(buttonRest);
+
+        expect(screen.getByText('4')).toBeDefined();
+
+    })
+
+    test('should increase count when -1 button is pressed and quantity is 1', () => { 
+        
+        const quantity = 1;
+
+        render(<ItemCounter name={'Test item'} quantity={quantity} />)
+
+        const [,buttonRest] = screen.getAllByRole('button');
+        
+        fireEvent.click(buttonRest);
+
+        expect(screen.getByText('1')).toBeDefined();
+
+    })
+
+});
