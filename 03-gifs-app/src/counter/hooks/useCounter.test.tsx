da8e83bf95bc38a7234d1a70aacdee83b3644e26
JuanCas <jcastro@colegium.com>
2025-12-01 15:15:53 -0300

    pruebas sobre custom hooks

---
+++ 03-gifs-app/src/counter/hooks/useCounter.test.tsx
@@ -0,0 +1,58 @@
+import { act, renderHook } from '@testing-library/react';
+import { beforeEach, describe, expect, test } from 'vitest';
+import { useCounter } from './useCounter';
+
+describe('useCounter', () => {
+    /* let result;
+    
+    beforeEach(() => {
+        const { result: hookValue } = renderHook(() => useCounter());
+        result = hookValue;
+    }) */
+
+    test('should intialize with default value of 10', () => {
+        const { result } = renderHook(() => useCounter());
+        expect(result.current.counter).toBe(10);
+    })
+
+    test('should intialize with default value of 20', () => {
+        const initialValue = 20;
+        const { result } = renderHook(() => useCounter(initialValue));
+        expect(result.current.counter).toBe(initialValue);
+    })
+
+    test('should increment counter when handleAdd is called', () => {
+        const { result } = renderHook(() => useCounter());
+        act(() => {
+            result.current.handleAdd()
+        })
+        expect(result.current.counter).toBe(11);
+    })
+
+    test('should decrement counter when handleSubstract is called', () => {
+        const { result } = renderHook(() => useCounter());
+        act(() => {
+            result.current.handleSubtract()
+        })
+        expect(result.current.counter).toBe(9);
+    })
+
+    test('should reset counter when handleReset is called', () => {
+        const { result } = renderHook(() => useCounter());
+
+        act(() => {
+            result.current.handleSubtract();
+            result.current.handleSubtract();
+            result.current.handleSubtract();
+            result.current.handleSubtract();
+            result.current.handleSubtract();
+        })
+        expect(result.current.counter).toBe(5);
+
+        act(() => {
+            result.current.handleReset();
+        })
+
+        expect(result.current.counter).toBe(10);
+    })
+})
