5423af6fd58ed80634149b4da7212d554dc75f0b
JuanCas <jcastro@colegium.com>
2025-12-05 12:45:38 -0300

    pruebas adiociones sobre efectos

--- 03-gifs-app/src/shared/components/SearchBar.test.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.test.tsx
@@ -23,7 +23,44 @@ describe('SearchBar', () => {
         waitFor(() => {
             expect(onQuery).toHaveBeenCalled();
             expect(onQuery).toHaveBeenCalledWith('test');
-        });
+        })
+    });
 
+    test('should call only once with the last value (debounce)', () => { 
+        const onQuery = vi.fn();
+        render(<SearchBar onQuery={onQuery} />);
+
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input, {target: {value: 't'}});
+        fireEvent.change(input, {target: {value: 'te'}});
+        fireEvent.change(input, {target: {value: 'tes'}});
+        fireEvent.change(input, {target: {value: 'test'}});
+
+        waitFor(() => {
+            expect(onQuery).toHaveBeenCalledTimes(1);
+            expect(onQuery).toHaveBeenCalledWith('test');
+        })
+     });
+
+     test('should call onQuery when button click with the input value', () => { 
+        const onQuery = vi.fn();
+        render(<SearchBar onQuery={onQuery} />);
+
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input, {target: {value: 'test'}});
+
+        const button = screen.getByRole('button');
+        fireEvent.click(button);
+
+        expect(onQuery).toHaveBeenCalledTimes(1);
+        expect(onQuery).toHaveBeenCalledWith('test');
+      })
+
+      test('should the input has the correct placeholder value', () => { 
+        const value = 'Buscar gif';
+        
+        render(<SearchBar onQuery={()=>{}} placeholder={value} />);
+
+        expect(screen.getByPlaceholderText(value)).toBeDefined();
     })
 })
