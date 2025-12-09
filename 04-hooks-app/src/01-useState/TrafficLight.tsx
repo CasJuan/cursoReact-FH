68b269f50488c5b3927fc1bce7d3d09940f3b5b8
JuanC <castrojuanig25@gmail.com>
2025-12-09 12:08:22 -0300

    useState- estado de redibuja

---
+++ 04-hooks-app/src/01-useState/TrafficLight.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Onclick } from '../../../03-gifs-app/src/gifs/intefaces/giphy.response';
+
+const colors = {
+    red: 'bg-red-500 animate-pulse',
+    yellow: 'bg-yellow-500 animate-pulse',
+    green: 'bg-green-500 animate-pulse',
+}
+
+export const TrafficLight = () => {
+
+    const [light, setLight] = useState('red');
+
+    const handleColorChange = (color: string) => {
+        setLight((prev) => {
+            console.log({prev})
+            return color;
+        });
+    }
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+      <div className="flex flex-col items-center space-y-8">
+
+        <div className={`w-32 h-32 ${light === 'red' ? colors[light]: 'bg-gray-500' } rounded-full`}></div>
+        <div className={`w-32 h-32 ${light === 'yellow' ? colors[light]: 'bg-gray-500' } rounded-full`}></div>
+        <div className={`w-32 h-32 ${light === 'green' ? colors[light]: 'bg-gray-500' } rounded-full`}></div>
+
+        <div className="flex gap-2">
+          <button
+            onClick={() => handleColorChange('red')}
+            className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer">
+            Rojo
+          </button>
+          <button
+            className="bg-yellow-500 text-white px-4 py-2 rounded-md cursor-pointer"
+            onClick={() => handleColorChange('yellow')}
+            >
+            Amarillo
+          </button>
+          <button
+            className="bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer"
+            onClick={() => handleColorChange('green')}
+            >
+            Verde
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
