6d79a74239fab55d873250b52b1a37c53543079f
JuanC <castrojuanig25@gmail.com>
2025-12-09 11:54:17 -0300

    tailwind y estilos

--- 04-hooks-app/src/HooksApp.tsx
+++ 04-hooks-app/src/HooksApp.tsx
@@ -1,6 +1,8 @@
 export const HooksApp = () => {
   return (
-    <div><h1>Hola Mundo</h1></div>
+    <div className="bg-gradient">
+        <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+    </div>
   )
 }
 
