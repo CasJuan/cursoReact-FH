9d51d7ab557bc4ebb20c900ef045e7a2d925fe86
JuanCas <jcastro@colegium.com>
2025-12-11 16:25:36 -0300

    efecto de confetti

--- 04-hooks-app/src/05-useReducer/ScrambleWords.tsx
+++ 04-hooks-app/src/05-useReducer/ScrambleWords.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Card, CardContent } from '@/components/ui/card';
@@ -23,6 +23,16 @@ export const ScrambleWords = () => {
         skipCounter,
         totalWords
     } = state;
+
+
+    useEffect(() => {
+        if(points === 0 )return;
+        confetti({
+            particleCount: 100,
+            spread: 120,
+            origin: {y: 0.6}
+        })
+    }, [points])
 
   const handleGuessSubmit = (e: React.FormEvent) => {
     e.preventDefault();
